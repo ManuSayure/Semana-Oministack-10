b6b9ce714e3998a35c26cebaa1abc32573a1fa4f
Manoela Cassia <manoelacs@ic.ufal.br>
2020-12-23 18:40:52 -0300

    adicionando rotas de busca de devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,30 +1,45 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
-module.exports =  {
-    async store(req, res){
+const { index } = require('../models/utils/PointSchema');
 
-        console.log(req.body);
+//index, store, show, update, destroy  
+
+module.exports =  {
+    async index(req, res){
+
+        const devs =  await Dev.find();
+        console.log(devs);
+
+        return res.json(devs);
+
+    },
+    async store(req, res){        
     
         const { github_username, techs, latitude, longitude } = req.body;
-         apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-       
-        console.log(apiResponse.data);
-        const {name = login, avatar_url , bio } = apiResponse.data;
-        const techsArray = techs.split(',').map((tech) => tech.trim())
-        console.log(techsArray);
-        const location = {
-            type: 'Point',
-            coordinates: [ longitude, latitude]
-        }
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs:techsArray,
-            location,
+
+        //Checar se o registro já existe
+        let dev = await Dev.findOne({github_username});
+        if(!dev){
+            apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);     
     
-        });
+            const {name = login, avatar_url , bio } = apiResponse.data;
+    
+            const techsArray = techs.split(',').map((tech) => tech.trim())
+    
+            const location = {
+                type: 'Point',
+                coordinates: [ longitude, latitude]
+            }
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs:techsArray,
+                location,
+        
+            });
+        }       
         return(
             res.json(dev)
         );
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,14 @@
 //import {Router} from 'express';
 const {Router} = require('express');
-const {store}=  require('../src/controllers/DevController');
+
+const {store, index}=  require('../src/controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
 routes.post('/devs', store);
+routes.get('/devs', index);
+
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
