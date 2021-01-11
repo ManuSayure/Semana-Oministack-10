d59508dde9726df4d951e5776c52becf55c6bc05
Manoela Cassia <manoelacs@ic.ufal.br>
2021-01-10 23:17:20 -0300

    atualizando

--- web/src/components/Loading.js
+++ web/src/components/Loading.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Paragrafo = styled.h1`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    mn-height: 100vh;
+    margin: 100 50;
+    padding: 30 30; 
+    
+    font-size: 1.5em;
+    text-align: center;
+    color: #7d40e7;
+`;
 
 const Loading = () => {
     return(
-        <div className="col-12">
-            <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
-            <p>Loading . . .</p>
+        <div >
+            
+         <Paragrafo> 
+             <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
+            Loading . . . 
+        </Paragrafo>
         </div>
     );
 }; export default Loading;
--- web/src/index.js
+++ web/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
-//import '@fortawesome/fontawesome-free/css/font-awesome.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js'
+import '@fortawesome/fontawesome-free/js/brands.js'
 
 ReactDOM.render(
   <React.StrictMode>
